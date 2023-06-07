e3204504c46dbc05e7bd195b7e59353519fdcc72
Jean-pierre14 <chiruzabisimwa@gmail.com>
2023-06-07 21:51:05 +0300

    Sales by match

--- index.cpp
+++ index.cpp
@@ -1,10 +1,35 @@
-
 #include <iostream>
+#include <Poco/Net/HTTPServer.h>
+#include <Poco/Net/ServerSocket.h>
 
-int main (){
-    auto result = 34;
-    for(int i = 0; i < 5; i++){
-        std::cout << "i = " << i ;
+using namespace Poco;
+using namespace Poco::Net;
+
+class MyRequestHandler : public HTTPRequestHandler
+{
+public:
+    void handleRequest(HTTPServerRequest& request, HTTPServerResponse& response)
+    {
+        if (request.getMethod() == "GET" && request.getURI() == "/hello")
+        {
+            response.setChunkedTransferEncoding(true);
+            response.setContentType("text/plain");
+            std::ostream& ostr = response.send();
+            ostr << "Hello, world!";
+        }
+        else
+        {
+            response.setStatus(HTTPResponse::HTTP_NOT_FOUND);
+            response.setReason("Not Found");
+        }
     }
+};
+
+int main(int argc, char** argv)
+{
+    HTTPServer server(new MyRequestHandlerFactory, ServerSocket(8080), new HTTPServerParams);
+    server.start();
+    std::cout << "Server started" << std::endl;
+    server.waitForStop();
     return 0;
 }
